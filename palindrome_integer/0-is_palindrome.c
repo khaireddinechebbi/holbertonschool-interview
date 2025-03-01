442b14f86a8d6cc17ebc08f166339d9c26be852d
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2025-03-01 08:19:35 +0100

    is palindrome

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,18 @@
+#include "palindrome.h"
+/**
+* is_palindrome - Checks if a given unsigned integer is a palindrome
+* @n: The number to check
+*
+* Return: 1 if n is a palindrome, 0 otherwise
+*/
+
+int is_palindrome(unsigned long n)
+{
+    unsigned long reversed = 0, temp = n;
+    while (temp != 0)
+    {
+        reversed = reversed * 10 + (temp % 10);
+        temp /= 10;
+    }
+    return (n == reversed);
+}
