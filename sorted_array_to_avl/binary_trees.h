bf25b42451f1142157ebd85e687eae94a3a5567b
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2025-04-06 14:43:49 +0200

    betty style

--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -8,13 +8,13 @@
 /* ===================== STRUCTURES ===================== */
 
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
+* struct binary_tree_s - Binary tree node
+*
+* @n: Integer stored in the node
+* @parent: Pointer to the parent node
+* @left: Pointer to the left child node
+* @right: Pointer to the right child node
+*/
 struct binary_tree_s
 {
 int n;
