f8b537158af58c6a7ac24667d1a2996f5ea323aa
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2025-04-18 00:38:48 +0200

    update

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -1,47 +1,49 @@
 #include "search.h"
 
 /**
- * linear_skip - searches for a value in a sorted skip list
- * @list: pointer to the head of the skip list
- * @value: value to search for
- * Return: pointer to the first node where value is located, or NULL
- */
+* linear_skip - searches for a value in a sorted skip list
+* @list: pointer to the head of the skip list
+* @value: value to search for
+* Return: pointer to the first node where value is located, or NULL
+*/
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *node, *jump;
+skiplist_t *node, *jump;
 
-	if (list == NULL)
-		return (NULL);
+if (list == NULL)
+return (NULL);
 
-	node = list;
-	jump = list->express;
+node = list;
+jump = list->express;
 
-	while (jump != NULL)
-	{
-		printf("Value checked at index [%lu] = [%d]\n", jump->index, jump->n);
-		if (jump->n >= value)
-			break;
-		node = jump;
-		jump = jump->express;
-	}
+while (jump != NULL)
+{
+printf("Value checked at index [%lu] = [%d]\n", jump->index, jump->n);
+if (jump->n >= value)
+break;
+node = jump;
+jump = jump->express;
+}
 
-	if (jump == NULL)
-	{
-		/* No more express lanes, go to the end of the list */
-		jump = node;
-		while (jump->next)
-			jump = jump->next;
-	}
+if (jump == NULL)
+{
+/* No more express lanes, go to the end of the list */
+jump = node;
+while (jump->next)
+jump = jump->next;
+}
 
-	printf("Value found between indexes [%lu] and [%lu]\n", node->index, jump->index);
+printf(
+"Value found between indexes [%lu] and [%lu]\n", node->index, jump->index
+);
 
-	while (node && node->index <= jump->index)
-	{
-		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
-		if (node->n == value)
-			return (node);
-		node = node->next;
-	}
+while (node && node->index <= jump->index)
+{
+printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+if (node->n == value)
+return (node);
+node = node->next;
+}
 
-	return (NULL);
+return (NULL);
 }
