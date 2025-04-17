496c1f3b15ef1a5f07e931b42a23ab35a0222e37
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2025-04-18 00:40:20 +0200

    update

--- linear_skip/search.h
+++ linear_skip/search.h
@@ -8,22 +8,22 @@
 
 
 /**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
- */
+* struct skiplist_s - Singly linked list with an express lane
+*
+* @n: Integer
+* @index: Index of the node in the list
+* @next: Pointer to the next node
+* @express: Pointer to the next node in the express lane
+*
+* Description: singly linked list node structure with an express lane
+* for Holberton project
+*/
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+int n;
+size_t index;
+struct skiplist_s *next;
+struct skiplist_s *express;
 } skiplist_t;
 
 skiplist_t *create_skiplist(int *array, size_t size);
